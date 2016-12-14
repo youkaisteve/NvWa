815dc02d75654614a9fd47ef592ebb064ba37329
youkaisteve <youkaisteve@gmail.com>
2016-12-14 16:30:46 +0800

    fix bug in autoNorm:minus minVals with a empty dataSet,not the original dataSet

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import operator
+import random
 
 
 def createDataSet():
@@ -41,3 +42,56 @@ def file2matrix(filename):
         classLabelVector.append(int(splitlines[-1]))
         index += 1
     return returnMat, classLabelVector
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    m = dataSet.shape[0]
+    normalDataSet = dataSet - tile(minVals, (m, 1))
+    normalDataSet = normalDataSet / tile(ranges, (m, 1))
+
+    return normalDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.1
+    datingMat, labels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingMat)
+    m = normMat.shape[0]
+    testNumbers = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(testNumbers):
+        classifierResult = classify0(normMat[i, :], normMat[testNumbers:m, :], labels[testNumbers:m], 3)
+        print('the classify0 call result is %d, and the real result is %s' % (classifierResult, labels[i]))
+        if classifierResult != labels[i]:
+            errorCount += 1
+
+    print(errorCount / float(testNumbers))
+
+
+def classifyPerson():
+    resultList = ['not at all', 'in small does', 'in large does']
+    flyMiles = float(input('filer mile earned per year:'))
+    percentageOfGame = float(input('Percentage of time spent playing video games:'))
+    litersOfIceCream = float(input('Liters of ice cream consumed per week:'))
+
+    testPerson = array([flyMiles, percentageOfGame, litersOfIceCream])
+    datingMat, labels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingMat)
+    inArr = (testPerson - minVals) / ranges
+    result = classify0(inArr, normMat, labels, 10)
+    print('You will probably like this person:', resultList[result - 1])
+
+
+def autoClassifyPerson():
+    resultList = ['not at all', 'in small does', 'in large does']
+    datingMat, labels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingMat)
+    for i in range(100):
+        testPerson = array([random.uniform(1.0, 10000.0), random.uniform(1.0, 20.0), random.uniform(0.5, 2.0)])
+        inArr = (testPerson - minVals) / ranges
+        result = classify0(inArr, normMat, labels, 10)
+        print('Person in %f-%f-%f You will probably like this person:' % (testPerson[0], testPerson[1], testPerson[2]),
+              resultList[result - 1])
--- KNN/test.py
+++ KNN/test.py
@@ -1,4 +1,4 @@
-from kNN import createDataSet, classify0, file2matrix
+from kNN import *
 import matplotlib
 import matplotlib.pyplot as plt
 from numpy import *
@@ -6,20 +6,27 @@ from numpy import *
 # sli
 # group, labels = createDataSet()
 #
-# result = classify0([1, 1.2], group, labels, 3)
+# result = classify0([0, 0.2], group, labels, 3)
 #
 # print('result:%s' % result)
 
 # dating
-datingMat, datingLabels = file2matrix('datingTestSet2.txt')
+# datingMat, datingLabels = file2matrix('datingTestSet2.txt')
+#
+# normal, r, minVals = autNorm(datingMat)
 
-fig = plt.figure()
+# fig = plt.figure()
+#
+# ax = fig.add_subplot(111)
+#
+# ax.scatter(datingMat[:, 1], datingMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+# ax.scatter(datingMat[:, 0], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+# ax.axis([-2, 25, -0.2, 2.0])
+# plt.xlabel('Percentage of Time Spent Playing Video Games')
+# plt.ylabel('Liters of Ice Cream Consumed Per Week')
+#
+# plt.show()
 
-ax = fig.add_subplot(111)
+datingClassTest()
 
-ax.scatter(datingMat[:, 1], datingMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
-ax.axis([-2, 25, -0.2, 2.0])
-plt.xlabel('Percentage of Time Spent Playing Video Games')
-plt.ylabel('Liters of Ice Cream Consumed Per Week')
-
-plt.show()
+# autoClassifyPerson()
