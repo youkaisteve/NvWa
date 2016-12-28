463e6423d4238b1f9235f7485b853c8882287cec
youkaisteve <youkaisteve@gmail.com>
2016-12-28 17:41:56 +0800

    asyn

--- NaiveBayes/review_comparison/jd/jd_crawl.py
+++ NaiveBayes/review_comparison/jd/jd_crawl.py
@@ -6,6 +6,10 @@ import shutil
 import random
 import time
 import re
+import json
+import asyncio
+import aiohttp
+import html
 
 from requests import HTTPError
 from collections import deque
@@ -18,7 +22,8 @@ class JDCrawl():
     urlpattern = "unnamed.txt"
     totalpages = 1
     savefilename = "saved.txt"
-    queue = deque()
+    tasks = []
+    sem = asyncio.Semaphore(5)
 
     def __init__(self, bookname, urlpattern, totalpages):
         self.bookname = bookname
@@ -33,7 +38,7 @@ class JDCrawl():
             os.remove(self.savefilename)
 
         for i in range(0, totalpages):
-            self.queue.append(urlpattern % i)
+            self.tasks.append(urlpattern % i)
 
         print('初始化完成')
         print('---------------------')
@@ -48,18 +53,18 @@ class JDCrawl():
     def extract_and_save(self, ajaxContent, index):
         formatStr = '%s\t%s\t%s\t%s\t%s\t%s\t%s\n'
 
-        reviewlist = ajaxContent.comments
+        reviewlist = ajaxContent['comments']
 
         book = open(os.path.join(self.bookname, str(index) + '.txt'), 'w')
 
         for review in reviewlist:
-            title = review.title
-            rating = review.score
-            content = review.content
-            author = review.nickname
-            time = review.creationTime
-            province = review.userProvince
-            userlevel = review.userLevelName
+            title = review['title']
+            rating = review['score']
+            content = str(review['content']).replace('\n', ' ')
+            author = review['nickname']
+            time = review['creationTime']
+            province = review['userProvince']
+            userlevel = review['userLevelName']
             book.write(formatStr % (rating, author, time, title, content, userlevel, province))
 
         book.close()
@@ -72,49 +77,51 @@ class JDCrawl():
                     with open(os.path.join(self.bookname, filename)) as src:
                         shutil.copyfileobj(src, dest)
                         src.close()
-                        os.remove(os.path.join(self.bookname, filename))
+                        # os.remove(os.path.join(self.bookname, filename))
         os.rename(os.path.join(self.bookname, tempfilename), self.bookname + ".txt")
 
-    def run(self):
-        visited = set()
-        cnt = 0
-
-        userAgent = [
-            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'
-        ]
-
-        headers = {
-            'User-Agent': random.choice(userAgent),
-            'Accept': 'text/html;charset=GBK'
-        }
-
-        opener = urllib.request.build_opener()
-        opener.addheaders = [headers]
-
-        while self.queue:
-            url = self.queue.popleft()
-            visited |= {url}
-            cnt += 1
-            print('已经抓取: ' + str(cnt) + '   正在抓取 <---  ' + url)
-
-            # 避免程序异常中止, 用try..catch处理异常
+    @asyncio.coroutine
+    def fetch(self, url, cnt):
+        client = aiohttp.ClientSession()
+        with (yield from self.sem):
             try:
-                urlop = opener.open(url)
-                data = urlop.read().decode('GBK')
-                # print(data)
-                print(re.findall(r'(?<=\()\S*(?=\))', str(data)))
-                self.extract_and_save(data, cnt)
-            except HTTPError as e:
-                if e.code == 408:
-                    print('timeout occurs,sleep 60s')
-                    time.sleep(60)
-                continue
+                response = yield from client.get(url)
+                body = yield from response.text()
+                client.close()
+                objecrStr = re.findall(r'\{.*\}', str(body))[0]
+                object = json.loads(objecrStr)
+                self.extract_and_save(object, cnt)
             except Exception as e:
                 print(e)
 
+    def run(self):
+        f = asyncio.wait([self.fetch(task, self.tasks.index(task) + 1) for task in self.tasks])
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(f)
+
+        # while self.queue:
+        #     url = self.queue.popleft()
+        #     visited |= {url}
+        #     cnt += 1
+        #     print('已经抓取: ' + str(cnt) + '   正在抓取 <---  ' + url)
+        #
+        #     # 避免程序异常中止, 用try..catch处理异常
+        #     try:
+        #         urlop = opener.open(url)
+        #         data = urlop.read().decode('GBK')
+        #         # print(data)
+        #         objecrStr = re.findall(r'\{.*\}', str(data))[0]
+        #         object = json.loads(objecrStr)
+        #         self.extract_and_save(object, cnt)
+        #     except HTTPError as e:
+        #         if e.code == 408:
+        #             print('timeout occurs,sleep 60s')
+        #             time.sleep(60)
+        #         continue
+        #     except Exception as e:
+        #         print(e)
+        loop.close()
+        print('closed')
         self.combineFiles('*.txt')
         self.dispose()
 
@@ -122,6 +129,6 @@ class JDCrawl():
 jdcrawl = JDCrawl(
     '解忧杂货铺',
     'https://club.jd.com/comment/skuProductPageComments.action?productId=11452840&score=0&sortType=3&page=%d&pageSize=10&isShadowSku=0&callback=fetchJSON_comment98vv61564',
-    1)
+    10)
 
 jdcrawl.run()
